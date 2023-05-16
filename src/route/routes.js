80a26885b9fba1d6066d1cb8e4aae0cc1d8e0a46
theProficient <theproficienttech333@gmail.com>
2023-05-16 17:34:12 +0530

    kjdhdj

--- src/route/routes.js
+++ src/route/routes.js
@@ -50,11 +50,11 @@ Router.get("/getAllHockyData", hockyController.getAllHoc);
 
 Router.post("/snktournaments", snakeLadderController.createSnakeLadderTables);
 
-Router.get("/tables", snakeLadderController.getAllSnak);
+Router.get("/getAllSnakeLadderData", snakeLadderController.getAllSnak);
 
-Router.put("/tournament", snakeLadderController.updateSnakLdrTournaments);
+Router.put("/updateSnakeLadder", snakeLadderController.updateSnakLdrTournaments);
 
-Router.get("/groups", snakeLadderController.getSnkByGroupId);
+Router.get("/getGroup", snakeLadderController.getSnkByGroupId);
 
 Router.put("/updateSnakeLadderPerPlayer", snakeLadderController.updatePointOfUser);
 
