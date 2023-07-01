ad05a5fbe92ab34cf8a0b50fa226054ac0b9ea76
theProficient <theproficienttech333@gmail.com>
2023-07-01 18:52:31 +0530

    delete dotenv file

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-require("dotenv").config();
+
 const express = require("express");
 const app = express();
-const cors = require("cors");
+// const cors = require("cors");
 
 const mongoose = require("mongoose");
 const route = require("./route/routes");
 
 app.use(express.json());
-app.use(cors());
+//app.use(cors());
 
 app.use("/", route);
 
@@ -15,38 +15,38 @@ mongoose.set("strictQuery", false);
 
 //____________________for development____________________________________
 
-// mongoose
-//   .connect(
-//     "mongodb+srv://theproficienttech333:gzYGYI5pD4oAUvim@cluster0.gp7jlnb.mongodb.net/game",
-//     {
-//       useNewUrlParser: true,
-//     }
-//   )
-//   .then(() => {
-//     console.log("MongoDB is connected for development");
-//   })
-//   .catch((error) => {
-//     console.log("Not connected");
-//   });
-// app.listen(process.env.PORT || 8000, function () {
-//   console.log("Express app running on port" + " " +(process.env.port || 8000));
-// });
-
-  //_________________________for local________________________
-
-  mongoose
+mongoose
   .connect(
-    "mongodb+srv://nikita1:7CSKh9nBmgBm27YC@cluster0.suzof1p.mongodb.net/nikita",
+    "mongodb+srv://theproficienttech333:gzYGYI5pD4oAUvim@cluster0.gp7jlnb.mongodb.net/game",
     {
       useNewUrlParser: true,
     }
   )
   .then(() => {
-    console.log("MongoDB is connected for local");
+    console.log("MongoDB is connected for development");
   })
   .catch((error) => {
     console.log("Not connected");
   });
+app.listen(process.env.PORT || 8000, function () {
+  console.log("Express app running on port" + " " +(process.env.port || 8000));
+});
+
+  //_________________________for local________________________
+
+  // mongoose
+  // .connect(
+  //   "mongodb+srv://nikita1:7CSKh9nBmgBm27YC@cluster0.suzof1p.mongodb.net/nikita",
+  //   {
+  //     useNewUrlParser: true,
+  //   }
+  // )
+  // .then(() => {
+  //   console.log("MongoDB is connected for local");
+  // })
+  // .catch((error) => {
+  //   console.log("Not connected");
+  // });
 
 
 app.listen(process.env.PORT || 5000, function () {
